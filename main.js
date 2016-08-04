8652b43d3e627cf58d4c9194afa38e3eb742339b
Andrew Chalkley <andrew@chalkley.org>
2016-08-04 16:40:02 -0700

    Updating to import json

--- main.js
+++ main.js
@@ -4,15 +4,13 @@ import {init as matrix, context, render} from 'thingssdk-adafruit-matrix';
 import {init as face, setState as setFaceState} from './face';
 import {init as wifimanager} from './wifimanager';
 import {request} from './api';
+import credentials from './credentials.json';
 
 matrix({scl:5, sda:4, address:0x70, brightness: 0});
 
 face(context, render);
 
 wifimanager({
-    credentials: {
-        "SSID": "PASSWORD",
-        "ANOTHER_SSID": "THAT_OTHER_PASSWORD"
-    },
+    credentials, 
     onConnect: () => request(setFaceState)
 });
